5f137d93f1af82ffdbf1d48500f18af0f489deca
Sva76 <simona.vargiu.malta@gmail.com>
2025-12-13 16:42:04 +0100

    Add make_plots.py to generate loss vs step plots

---
+++ experiments/make_plots.py
@@ -0,0 +1,3 @@
+python experiments/run_killer_shock.py --vincolo off --amplify_reported_shock_baseline
+python experiments/run_killer_shock.py --vincolo on
+python experiments/make_plots.py --out results/loss_vs_step_killer.png
